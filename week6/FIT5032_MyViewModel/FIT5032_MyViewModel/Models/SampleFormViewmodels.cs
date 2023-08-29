25b6a98e97efc53be877adb9b73a4f499465f5b6
hlin0067 <hlin0067@MV2NDCSTUL05027>
2023-08-29 11:08:14 +1000

    week6

---
+++ week6/FIT5032_MyViewModel/FIT5032_MyViewModel/Models/SampleFormViewmodels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+namespace FIT5032_MyViewModel.Models
+{
+    public class SampleFormViewmodels
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
