3cfca7532a9b4b809869bc85287f052d386f5871
hlin0067 <hlin0067@MV2NDCSTUL05027>
2023-09-15 13:53:57 +1000

    new

---
+++ MyAssignment/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyAssignment.MvcApplication" Language="C#" %>
---
+++ MyAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAssignment.Startup))]
+namespace MyAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
