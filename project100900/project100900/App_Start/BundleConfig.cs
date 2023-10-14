a020a0146df55fb720d20dd353591a0c1bbf323e
hlin0067 <hlin0067@MV2NDCSTUL05027>
2023-10-14 19:16:51 +1100

    map

--- project100900/project100900/App_Start/BundleConfig.cs
+++ project100900/project100900/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace project100900
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/mapbox").Include(
+                        "~/Scripts/location.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
